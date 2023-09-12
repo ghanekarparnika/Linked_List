a8cd087a70a835ad43803debcafa82f8dadf8f34
ghanekarparnika <parnikaghanekar@gmail.com>
2023-09-12 16:08:15 +0530

    [parnika]:Program to ctrate LinkedList by adding node

---
+++ Linked_List/LinkedList_Demo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linked_List
+{
+    internal class LinkedList_Demo
+    {
+        public Node head;
+        public void addNode(int data)
+        {
+            Node node = new Node(data);
+            if(this.head == null)
+            {
+                this.head = node;
+            }
+            else 
+            {
+                Node temp = head;
+                while(temp.Next != null) 
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine(data+" is inserted into the linkedlist");
+        }
+
+        public void Display()
+        {
+            if(head == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+            }
+            else 
+            {
+                Node tempNode = head;
+                while(tempNode != null) 
+                {
+                    Console.Write(tempNode.data + "-->");
+                    tempNode = tempNode.Next;
+                }
+            }
+        }
+
+    }
+}
